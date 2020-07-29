53da66aa0250f4ef57a26cea0f0a6a846e76ebb4
PratthamArora <arora.prattham@gmail.com>
2020-07-29 22:09:05 +0530

    pick image from local storage

--- app/src/main/java/com/pratthamarora/facedetection_mlkit/MainActivity.kt
+++ app/src/main/java/com/pratthamarora/facedetection_mlkit/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.pratthamarora.facedetection_mlkit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+        const val IMAGE_PICKER = 100
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectImageBtn.setOnClickListener {
+            getImageFromLocal()
+        }
+
+    }
+
+    private fun getImageFromLocal() {
+        Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+            putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+            startActivityForResult(this, IMAGE_PICKER)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_PICKER) {
+            if (resultCode == Activity.RESULT_OK) {
+                val uri = data?.data
+                val bitmap = uri?.let { getBitmap(it) }
+                imageViewFace.setImageBitmap(bitmap)
+            }
+        }
+    }
+
+    private fun getBitmap(uri: Uri): Bitmap {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            MediaStore.Images.Media.getBitmap(contentResolver, uri)
+        } else {
+            val source = ImageDecoder.createSource(contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
+        }
     }
 }
